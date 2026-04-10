926e4ab78b4161a53ee39e0032288cd8f25c3848
cassi35 <cgrsobral@gmail.com>
2026-04-10 15:54:26 -0300

    fix: fix url microsoft

--- src/modules/auth/auth.service.ts
+++ src/modules/auth/auth.service.ts
@@ -40,7 +40,7 @@ export class AuthService {
       case 'microsoft':
         url =
           `https://login.microsoftonline.com/common/oauth2/v2.0/authorize?` + // ← 'common' no lugar do TENANT_ID
-          `client_id=${process.env.MICROSOFT_LOGIN}` +
+          `client_id=${process.env.CLIENT_ID_AZURE}` +
           `&redirect_uri=${encodeURIComponent(process.env.MICROSOFT_AUTH_URL!)}` +
           `&response_type=code` +
           `&scope=openid%20profile%20email%20User.Read` +
