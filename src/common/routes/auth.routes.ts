6b30b6237a97e77914dfa501c754f64a005473cf
cassi35 <cgrsobral@gmail.com>
2026-05-04 10:17:19 -0300

    feat: tive ideia de colocar rotas em um arquivo pra evitar erro de escrita

---
+++ src/common/routes/auth.routes.ts
@@ -0,0 +1,14 @@
+enum AuthRouter {
+  BASE = 'auth',
+  LOGIN = 'login',
+  SIGNUP = 'signup',
+  LOGOUT = 'logout',
+  VERIFY_EMAIL = 'verify-email',
+  FORGOT_PASSWORD = 'forgot-password',
+  RESET_PASSWORD = 'reset-password',
+  GOOGLE = 'google',
+  MICROSOFT = 'microsoft',
+  GOOGLE_CALLBACK = 'google/callback',
+  MICROSOFT_CALLBACK = 'microsoft/callback',
+}
+export default AuthRouter;
